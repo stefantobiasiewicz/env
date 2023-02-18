50f541ad86f47545a998772b3f738fa1cfdf2300
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-18 15:47:24 +0100

    deploy auth

--- build.sh
+++ build.sh
@@ -14,3 +14,12 @@ echo Building Cloud-iot-simulator
 mvn clean package -P simulator -DskipTests
 sudo docker rmi -f cloud_iot_simulator_image
 sudo docker build -f ./Dockerfile-sim -t cloud_iot_simulator_image .
+
+cd ../terra_cloud_auth/auth
+pwd
+
+git pull
+
+mvn clean package -DskipTests
+sudo docker rmi -f cloud_auth_image
+sudo docker build -t cloud_auth_image .
