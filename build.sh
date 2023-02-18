12beda849987a667a3f1cd2f6fba71ec9ab9c81d
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-18 12:59:33 +0100

    sudo

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo Building Cloud-iot
 cd ../terra_cloud_iot
 pwd
 mvn clean package -P service -DskipTests
-docker rmi -f cloud_iot_image
-docker build -t cloud_iot_image .
+sudo docker rmi -f cloud_iot_image
+sudo docker build -t cloud_iot_image .
 
 echo Building Cloud-iot-simulator
 mvn clean package -P simulator -DskipTests
-docker rmi -f cloud_iot_simulator_image
-docker build -f ./Dockerfile-sim -t cloud_iot_simulator_image .
+sudo docker rmi -f cloud_iot_simulator_image
+sudo docker build -f ./Dockerfile-sim -t cloud_iot_simulator_image .
