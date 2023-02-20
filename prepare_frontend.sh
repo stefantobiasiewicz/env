7d23181f40ba87d52584cdf5687cfc6847ba6e2a
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-20 23:53:05 +0100

    Frontend config v4

--- prepare_frontend.sh
+++ prepare_frontend.sh
@@ -3,3 +3,6 @@ echo Creating nginx config
 
 sudo cp nginx/frontend.conf /etc/nginx/conf.d/frontend.conf
 sudo cp nginx/simulator.conf /etc/nginx/conf.d/simulator.conf
+
+sudo cp -r nginx/ssl/www.tai.pl.csr /etc/nginx/ssl/www.tai.pl.csr
+sudo cp -r nginx/ssl/www.tai.pl.key /etc/nginx/ssl/www.tai.pl.key
