c1accd6467e75d2259e1634e78ddc6ca9c20945b
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-21 00:19:23 +0100

    Frontend config v13

--- prepare_frontend.sh
+++ prepare_frontend.sh
@@ -6,5 +6,6 @@ sudo cp nginx/simulator.conf /etc/nginx/conf.d/simulator.conf
 
 echo Copying ssl keys
 sudo mkdir -p /etc/nginx/ssl
+sudo cp -r nginx/ssl/dhparam.pem /etc/nginx/ssl/dhparam.pem
 sudo cp -r nginx/ssl/nginx-selfsigned.crt /etc/nginx/ssl/nginx-selfsigned.crt
 sudo cp -r nginx/ssl/nginx-selfsigned.key /etc/nginx/ssl/nginx-selfsigned.key
