7b7ff51373a38a39ab1ac57512263957d2490511
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-20 23:57:39 +0100

    Frontend config v6

--- prepare_frontend.sh
+++ prepare_frontend.sh
@@ -4,6 +4,7 @@ echo Creating nginx config
 sudo cp nginx/frontend.conf /etc/nginx/conf.d/frontend.conf
 sudo cp nginx/simulator.conf /etc/nginx/conf.d/simulator.conf
 
+echo Copying ssl keys
 sudo mkdir -p /etc/nginx/ssl
 sudo cp -r nginx/ssl/www.tai.pl.csr /etc/nginx/ssl/www.tai.pl.csr
 sudo cp -r nginx/ssl/www.tai.pl.key /etc/nginx/ssl/www.tai.pl.key
