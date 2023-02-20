6c569a1358ef795a54016262c382ba30ac98fc32
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-20 23:32:57 +0100

    Frontend config v2

--- deploy_forntend.sh
+++ deploy_forntend.sh
@@ -6,13 +6,17 @@ pwd
 git pull
 
 cd Tai-sim-frontend
+echo Coying simulator dist folder
 sudo cp -R dist /var/www/html/sim-frontend
+
 
 echo Pulling frontend
 cd ../terra_web
 pwd
 
 git pull
+
+echo Coying frontend dist folder
 sudo cp -R dist /var/www/html/frontend
 
 echo Restarting nginx
